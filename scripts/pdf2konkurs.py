75581c7574f7e6f1dcab46797af7d27213b9a2be
Alexander <a@perep.ru>
2017-10-26 16:45:55 +0300

    added konkurs problems text extraction

--- scripts/pdf2konkurs.py
+++ scripts/pdf2konkurs.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 #import toml
 import os
+import re
 import datetime
 import itertools
 from subprocess import call, check_call
@@ -25,6 +26,32 @@ def extract(filename):
             break
         call('bash -c " composite -compose CopyOpacity {1} {0} {2}"'.format(raw_file(i), raw_file(i+1), directory+""+str(i//2)+'.png'), shell=True)
         call('bash -c " rm {0} {1}"'.format(raw_file(i), raw_file(i+1)), shell=True)
+    
+    text_file = directory+'konkurs.txt'
+    call('bash -c "pdf2txt -o {0} -p 34,35 {1} "'.format(text_file,filename), shell=True)
+    with open(text_file,'r') as text:
+        problems = text.readlines()
+    tour_string_num = next(i for i, string in enumerate(problems) if "ТУР" in string)
+    problems = problems[tour_string_num+1:]
+    while(problems[0].strip()==''):
+        problems.pop(0)
+    firstnum = int(re.match( r'\d+', problems[0].strip() ).group(0))
+    print(firstnum)
+
+    def tour_template(firstnum):
+        template = ['tour: {}\n'.format(firstnum//5+1),'  number:\n','  problems:\n']    
+        for i in range(firstnum, firstnum+5):
+            template.append("""
+  - image_art: ''
+    image_scheme: ''
+    number: {}
+    problem: ''
+            """.format(i))
+        template.append('\n')
+        return template
+    with open(text_file,'w') as text:        
+        text.writelines(tour_template(firstnum))
+        text.writelines(problems)
 
 
 extract('local/2017-10.pdf')
