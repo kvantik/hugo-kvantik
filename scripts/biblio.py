3d1b4d7bf4ca890909e108d623b98c2c2346f5e2
Alexander Perepechko <a@perep.ru>
2020-01-30 15:48:25 +0300

    up

--- scripts/biblio.py
+++ scripts/biblio.py
@@ -1,24 +1,24 @@
 import common
 
+def get_articles(tag):
+    tables = {year:common.get_table(year) for year in range(2012, common.year+1)}
+    articles = [{**i, **{'год':y}} for y, t in tables.items() for i in t]
+    return [a for a in articles if tag in a['теги']]
 
 def make_pdf(tag, output_folder = None):
     if output_folder == None:
          output_folder = f'local/biblio-{tag}/'
     common.clear_folder(output_folder)
     issues_folder = common.pdfs()
-    
 
-    tables = {year:common.get_table(year) for year in range(2012, 2020)}
     pages_copied = 0
-    for year, table in tables.items():
-        for t in table:
-            if tag in t['теги']:
-                common.copy_article(issues_folder, year, 
+    for article in get_articles(tag):
+        common.copy_article(issues_folder, t['год'], 
                     t['выпуск'], t['первая'], t['всего'], 
                     output_folder, str(pages_copied).zfill(3))
-                pages_copied += int(t['всего'])
+        pages_copied += int(t['всего'])
     common.bash(f'pdftk {output_folder}* cat output {output_folder}biblio-{tag}.pdf')
-
+    return pages_copied
 
 if __name__ == "__main__":
     make_pdf(input('Введите тег для сбора в pdf: '))
